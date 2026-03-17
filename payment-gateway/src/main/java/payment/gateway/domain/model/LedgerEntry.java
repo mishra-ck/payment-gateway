abc1c8b80acea834af26e813b05f7c9c846f0f6b
Chanchal Mishra <chanchalimsec@gmail.com>
2026-03-17 23:41:40 +0530

    CR/DR logic added in Ledger and transaction

--- payment-gateway/src/main/java/payment/gateway/domain/model/LedgerEntry.java
+++ payment-gateway/src/main/java/payment/gateway/domain/model/LedgerEntry.java
@@ -3,6 +3,7 @@ package payment.gateway.domain.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.*;
 import payment.gateway.config.enums.EntryType;
 
 import java.math.BigDecimal;
@@ -11,10 +12,15 @@ import java.util.UUID;
 
 @Entity
 @Table(name = "ledger_entries")
+@Getter
+@NoArgsConstructor(access= AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
 public class LedgerEntry {
 
     @Id
     private UUID id;
+    private UUID journalId;
     private UUID paymentId;
     private UUID accountId;
     private EntryType entryType;
@@ -24,5 +30,30 @@ public class LedgerEntry {
     private boolean reversal = false;
     private UUID reversedEntryId;
     private Instant entryDate;
+
+    public static LedgerEntry debitEntry(UUID journalId, UUID paymentId, UUID accountId,
+        BigDecimal amount, String currency, String description){
+        return LedgerEntry.builder()
+                .journalId(journalId)
+                .paymentId(paymentId)
+                .accountId(accountId)
+                .entryType(EntryType.DR)
+                .amount(amount)
+                .currency(currency)
+                .description(description)
+                .build();
+    }
+    public static LedgerEntry creditEntry(UUID journalId, UUID paymentId, UUID accountId,
+                                         BigDecimal amount, String currency, String description){
+        return LedgerEntry.builder()
+                .journalId(journalId)
+                .paymentId(paymentId)
+                .accountId(accountId)
+                .entryType(EntryType.CR)
+                .amount(amount)
+                .currency(currency)
+                .description(description)
+                .build();
+    }
 
 }
