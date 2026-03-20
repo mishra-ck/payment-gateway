706e59b03234760b3baf4c026d5aaaee6cd2f6b0
Chanchal Mishra <chanchalimsec@gmail.com>
2026-03-20 23:57:13 +0530

    Updating service

--- payment-gateway/src/main/java/payment/gateway/service/AccountService.java
+++ payment-gateway/src/main/java/payment/gateway/service/AccountService.java
@@ -1,11 +1,19 @@
 package payment.gateway.service;
 
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import payment.gateway.saga.events.PaymentInitiatedEvent;
 
 @Service
+@Slf4j
 public class AccountService {
+    private final static Logger LOG = LoggerFactory.getLogger(AccountService.class);
+    @Transactional
     public void processDebit(PaymentInitiatedEvent event) {
-        /*TBD*/
+        LOG.info("SAGA_DEBIT_START : paymentId :{},account={}, amount={}, currency={}",
+          event.paymentId(),event.sourceAccountId(),event.amount(),event.currency());
     }
 }
