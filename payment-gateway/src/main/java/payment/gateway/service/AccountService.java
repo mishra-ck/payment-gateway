7975725c3d9b22812fb5b361b4ba580bf1edec76
Chanchal Mishra <chanchalimsec@gmail.com>
2026-03-22 23:14:39 +0530

    Updating AccountService

--- payment-gateway/src/main/java/payment/gateway/service/AccountService.java
+++ payment-gateway/src/main/java/payment/gateway/service/AccountService.java
@@ -6,15 +6,20 @@ import org.slf4j.LoggerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import payment.gateway.config.enums.TransactionType;
 import payment.gateway.domain.model.Account;
+import payment.gateway.domain.model.Transaction;
 import payment.gateway.exception.InvalidPaymentException;
+import payment.gateway.infrastructure.kafka.KafkaConfig;
 import payment.gateway.infrastructure.redis.DistributedLockService;
 import payment.gateway.repository.AccountRepository;
 import payment.gateway.repository.TransactionRepository;
+import payment.gateway.saga.events.PaymentDebitedEvent;
+import payment.gateway.saga.events.PaymentFailedEvent;
 import payment.gateway.saga.events.PaymentInitiatedEvent;
 import payment.gateway.saga.events.SagaEvent;
 
-import java.util.Optional;
+import java.time.Instant;
 
 @Service
 @Slf4j
@@ -45,17 +50,55 @@ public class AccountService {
                 Account debitAccount = account.get();
                 debitAccount.debit(event.amount());
                 accountRepository.save(debitAccount);
+
+                var txn = Transaction.debit(
+                        event.paymentId(),
+                        event.sourceAccountId(),
+                        event.amount(),
+                        event.currency(),
+                        debitAccount.getAvailableBalance(),
+                        event.correlationId()
+                );
+                transactionRepository.save(txn);
+
+                /* Publish Debit Event - Trigger Credit Event */
+                var debitEvent = new PaymentDebitedEvent(
+                        event.paymentId(),
+                        event.correlationId(),
+                        event.sourceAccountId(),
+                        event.destinationAccountId(),
+                        event.amount(),
+                        event.currency(),
+                        debitAccount.getAvailableBalance(),
+                        txn.getId(),
+                        Instant.now()
+                );
+
+                kafkaTemplate.send(
+                        KafkaConfig.TOPIC_PAYMENT_DEBITED,
+                        event.paymentId().toString(),debitEvent );
+
+                LOG.info("SAGA_DEBIT_OK : paymentId :{},txnId:{},balanceAfter:{}",
+                        event.paymentId(),txn.getId(),debitAccount.getAvailableBalance());
+
             }catch (Exception ex){
-
+                LOG.warn("SAGA_DEBIT_INSUFFICIENT_FUNDS : paymentId :{}, reason:{}",
+                        event.paymentId(),ex.getMessage());
+                kafkaTemplate.send(
+                        KafkaConfig.TOPIC_PAYMENT_FAILED,
+                        event.paymentId().toString(),
+                        new PaymentFailedEvent(
+                                event.paymentId(),
+                                event.correlationId(),
+                                "In-sufficient Funds: "+ex.getMessage(),
+                                TransactionType.DEBIT.name(),
+                                false,
+                                Instant.now()
+                        )
+                );
             }
-
             return null;
         });
-        try{
-
-        }catch (Exception ex){
-
-        }
-
     }
+
 }
