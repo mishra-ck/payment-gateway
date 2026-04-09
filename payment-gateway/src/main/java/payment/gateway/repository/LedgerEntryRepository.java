1a42dceef8e4e57728cbda95f411955fd73a9e40
Chanchal Mishra <chanchalimsec@gmail.com>
2026-04-09 20:44:10 +0530

    updated ledger repo

--- payment-gateway/src/main/java/payment/gateway/repository/LedgerEntryRepository.java
+++ payment-gateway/src/main/java/payment/gateway/repository/LedgerEntryRepository.java
@@ -1,12 +1,17 @@
 package payment.gateway.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import payment.gateway.domain.model.LedgerEntry;
 
+import java.math.BigDecimal;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface LedgerEntryRepository extends JpaRepository<LedgerEntry, UUID> {
      List<LedgerEntry> findByPaymentId(UUID paymentId);
-    /*TODO*/
+     List<LedgerEntry> findByJournalId(UUID journalId);
+     Optional<BigDecimal> checkJournalBalance(UUID journalId);
 }
