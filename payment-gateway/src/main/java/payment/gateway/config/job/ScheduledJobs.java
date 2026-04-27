941b9d8529a4877f8aa83377f1208d671f56ab9c
Chanchal Mishra <chanchalimsec@gmail.com>
2026-04-27 23:56:09 +0530

    Updating scheduled Jobs

--- payment-gateway/src/main/java/payment/gateway/config/job/ScheduledJobs.java
+++ payment-gateway/src/main/java/payment/gateway/config/job/ScheduledJobs.java
@@ -1,10 +1,34 @@
 package payment.gateway.config.job;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import payment.gateway.repository.IdempotencyRepository;
+import payment.gateway.repository.LedgerEntryRepository;
+import payment.gateway.repository.PaymentRepository;
 
 @Component
 @Slf4j
+@RequiredArgsConstructor
 public class ScheduledJobs {
-    /*TODO*/
+
+    private final IdempotencyRepository idempotencyRepository;
+    private final PaymentRepository paymentRepository;
+    private LedgerEntryRepository ledgerEntryRepository;
+    private final MeterRegistry meterRegistry;
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    // Idempotency Cleanup - every 6 Hrs
+    @Scheduled(fixedDelay = 6*3600*1000L, initialDelay = 60_000L)
+    @Transactional
+    public void cleanupExpiredIdempotencyRecords(){
+        /*TODO*/
+    }
 }
