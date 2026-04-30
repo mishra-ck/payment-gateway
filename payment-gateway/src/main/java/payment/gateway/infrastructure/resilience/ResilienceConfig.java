1d5e9a26cad2d9e92575ae4753dfe7ebec3e523b
Chanchal Mishra <chanchalimsec@gmail.com>
2026-04-30 23:35:15 +0530

    Resilience4j config file created

---
+++ payment-gateway/src/main/java/payment/gateway/infrastructure/resilience/ResilienceConfig.java
@@ -0,0 +1,13 @@
+package payment.gateway.infrastructure.resilience;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Resilience4j configuration for fault-tolerance
+ */
+@Configuration
+@Slf4j
+public class ResilienceConfig {
+    /*TODO*/
+}
